5a302c0d0a2ee770037ec4630c3916179f781586
Anastasiia <you@example.com>
2024-11-05 13:28:10 -0800

    first commit

---
+++ JS/csript.js
@@ -0,0 +1,13 @@
+"use strict"
+//const result = confirm("Are you here?");
+//console.log(result);
+
+/*const answer = +prompt ("are u 18?", "18");
+console.log (typeof(answer));*/
+
+const answers = [];
+answers[0] = prompt ("what is your name?");
+answers[1] = prompt ("what is your age?");
+answers[2] = prompt ("what is your city?");
+document.write(answers);
+ console.log(answers);
